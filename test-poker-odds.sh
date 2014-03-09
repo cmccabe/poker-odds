ab073b189b5194aaa82165d3ab0ab0b0dd4b3303
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2014-03-09 12:17:37 -0700

    update test-poker-odds.sh after off-by-one-fix
    commit 22555baa8972c8a85975188e65e18b5ffa5b19e4 fixed an off-by-one bug
    which was causing the generation of slightly incorrect odds because the
    last element of the futureBag was not being considered.  Update
    test-poker-odds.sh with the new odds and add another test.
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- test-poker-odds.sh
+++ test-poker-odds.sh
@@ -21,8 +21,8 @@ trap cleanup INT TERM EXIT
 "${poker_odds}" -a "KS QS" -b "AS 3S 5S" > "${tmp}"
 cat << EOF >  "${tmp2}"
 results:
-99.90% chance of a flush
-0.10% chance of a straight flush
+99.91% chance of a flush
+0.09% chance of a straight flush
 EOF
 diff "${tmp2}" "${tmp}" || die "unexpected result from test 1"
 
@@ -32,18 +32,26 @@ grep -q "100.00% chance of a straight flush" "${tmp}" || die "example 2 failed"
 "${poker_odds}" -a 'KC JC' -b '2S 3S 4S 5S' > "${tmp}"
 cat << EOF >  "${tmp2}"
 results:
-40.00% chance of nothing
-35.56% chance of a pair
-6.67% chance of a straight
-15.56% chance of a flush
-2.22% chance of a straight flush
+39.13% chance of nothing
+34.78% chance of a pair
+6.52% chance of a straight
+17.39% chance of a flush
+2.17% chance of a straight flush
 EOF
 diff "${tmp2}" "${tmp}" || die "unexpected result from test 3"
 
 "${poker_odds}" -b 'KD KC 5H' -a 'KS QS' > "${tmp}"
 cat << EOF >  "${tmp2}"
 results:
-95.65% chance of three of a kind
-4.35% chance of four of a kind
+95.74% chance of three of a kind
+4.26% chance of four of a kind
 EOF
 diff "${tmp2}" "${tmp}" || die "unexpected result from test 4"
+
+"${poker_odds}" -b "AS 7D 3D 4D" -a "KS QS" > "${tmp}"
+cat << EOF >  "${tmp2}"
+results:
+60.87% chance of nothing
+39.13% chance of a pair
+EOF
+diff "${tmp2}" "${tmp}" || die "unexpected result from test 5"
